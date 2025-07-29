2814f608067ca9b85033b35e51148e45f8726b9f
hrtowii <68852354+hrtowii@users.noreply.github.com>
2025-07-29 16:25:35 +0800

    feat: analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,17 @@ import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App.tsx";
 import WorkshopsPage from "./pages/workshops.tsx";
-
+import { Analytics } from "@vercel/analytics/next";
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/workshops" element={<WorkshopsPage />} />
-      </Routes>{" "}
-    </BrowserRouter>
-  </StrictMode>,
+  <>
+    <Analytics />
+    <StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/workshops" element={<WorkshopsPage />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
+  </>,
 );
