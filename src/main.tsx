f6af129be5f5cb7a0f0a1672cc080f22f2d4705e
hrtowii <68852354+hrtowii@users.noreply.github.com>
2025-07-29 16:27:17 +0800

    Revert "feat: analytics"
    This reverts commit 2814f608067ca9b85033b35e51148e45f8726b9f.

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,14 @@ import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App.tsx";
 import WorkshopsPage from "./pages/workshops.tsx";
-import { Analytics } from "@vercel/analytics/next";
+
 createRoot(document.getElementById("root")!).render(
-  <>
-    <Analytics />
-    <StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/workshops" element={<WorkshopsPage />} />
-        </Routes>
-      </BrowserRouter>
-    </StrictMode>
-  </>,
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/workshops" element={<WorkshopsPage />} />
+      </Routes>{" "}
+    </BrowserRouter>
+  </StrictMode>,
 );
